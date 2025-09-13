d1885fd53d6328aa661b30990c99d6899712659e
Nithin KV <gowdanithin232@gmail.com>
2025-09-13 22:31:27 +0530

    Add files via upload

---
+++ VeryBasic/P2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class P2 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n=sc.nextInt();
+    while(true){
+        if(n%10==0){
+            System.out.println("YOU ENTERED MULTIPLE OF 10");
+            continue;
+        }else{
+        System.out.println("enter again");
+    }
+}
+}
+}
