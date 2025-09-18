9dd7904cfbe96dea2fb0aa0d3a65b77428fdc838
Nithin KV <gowdanithin232@gmail.com>
2025-09-18 23:08:27 +0530

    Add files via upload

---
+++ 1.Variables&Datatypes/P2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class P2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a 1st number:");
+        int a= sc.nextInt();
+        System.out.print("Enter a 2nd number:");
+        int b= sc.nextInt();
+        System.out.println(a+"+"+b+"="+(a+b));
+    }
+}
