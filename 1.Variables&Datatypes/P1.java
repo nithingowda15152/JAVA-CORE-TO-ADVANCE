2a3160c574a12867af4c1c32a00e184677899c78
Nithin KV <gowdanithin232@gmail.com>
2025-09-18 23:07:24 +0530

    Add files via upload

---
+++ 1.Variables&Datatypes/P1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class P1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a string");
+        String s1=sc.nextLine();
+        System.out.println("s1="+s1);
+    }
+}
