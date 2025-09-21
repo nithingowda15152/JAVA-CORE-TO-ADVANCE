c978541c5c8b281a5f67061fe219c65be0ec496d
Nithin KV <gowdanithin232@gmail.com>
2025-09-21 19:20:24 +0530

    Add files via upload

---
+++ 1.Variables&Datatypes/TypeCasting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = 99.99f;
+        int b = (int)a;
+        System.out.println(b);
+    }
+}
+
---
+++ 1.Variables&Datatypes/input.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       //String input
+       //String input = sc.nextLine();
+       //System.out.println(input);
+
+       //Integer input
+       //int input = sc.nextInt();
+       //System.out.println(input);
+
+       //Float input
+       Float input = sc.nextFloat();
+       System.out.println(input);
+    }
+    
+}
