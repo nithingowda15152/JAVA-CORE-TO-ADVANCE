d2ed2a70cff3f08c6158d2fa0018f451920e6dac
Nithin KV <gowdanithin232@gmail.com>
2025-09-24 19:54:46 +0530

    Add files via upload

---
+++ 3.Loops/FibonacciSeries.java
@@ -0,0 +1,14 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+    int n=10;
+    int a=0;
+    int b=1;
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        System.out.println(a);
+        sum=a+b;
+        a=b;
+        b=sum;
+    }
+}
+}
