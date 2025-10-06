05c06ef78d4e196bccf9707653ab784e89aeab3a
Nithin KV <gowdanithin232@gmail.com>
2025-10-06 07:46:09 +0530

    Add files via upload

---
+++ 7.Casting/B.java
@@ -0,0 +1,9 @@
+public class B {
+    public static void main(String[] args) {
+        int n=(int)m1();
+        System.out.println(n);
+    }static double m1(){
+        double d=3.0;
+        return d;
+    }
+}
---
+++ 7.Casting/C.java
@@ -0,0 +1,10 @@
+public class C {
+    public static void main(String[] args) {
+        int n=(int)m1();
+        System.out.println(n);
+    }static double m1(){
+        double a=4.7;
+        double b=2.6;
+        return a+b;
+    }
+}
