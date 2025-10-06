24cd5bdc5d15ee0597cbb4d12688b1b75c215240
Nithin KV <gowdanithin232@gmail.com>
2025-10-06 07:45:27 +0530

    Add files via upload

---
+++ 7.Casting/A.java
@@ -0,0 +1,10 @@
+public class A{
+    public static void main(String[] args) {
+        // int n=15;
+        // double d=(double)n;
+        // System.out.print(d);//WIDENING
+        double d=15.00;
+        int n=(int)d;
+        System.out.print(n);//NARROWING
+    }
+}
