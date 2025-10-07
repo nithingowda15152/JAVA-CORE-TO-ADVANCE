e6b6c708da61c100a1de85050b2df5630cd3c00e
Nithin KV <gowdanithin232@gmail.com>
2025-10-07 08:15:47 +0530

    Add files via upload

---
+++ 8.Recursion(Basic)/A.java
@@ -0,0 +1,11 @@
+public class A {
+    public static void main(String[] args) {
+        m1(1);
+    }static void m1(int i){
+        System.out.println(i);
+        if(i>=3){
+            return ;
+        }m1(++i);
+        System.out.println(i);
+        }
+}
