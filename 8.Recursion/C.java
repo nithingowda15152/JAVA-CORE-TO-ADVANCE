4f93aed94e6ce4bd148536dd8b2b1fc0848e8b6d
Nithin KV <gowdanithin232@gmail.com>
2025-10-07 08:12:53 +0530

    Add files via upload

---
+++ 8.Recursion/C.java
@@ -0,0 +1,10 @@
+public class C {
+    public static void main(String[] args) {
+        display(1);
+    }static void display(int n){
+        if(n>10){
+            return;
+        }System.out.println(n);
+        display(n+1);
+    }
+}
